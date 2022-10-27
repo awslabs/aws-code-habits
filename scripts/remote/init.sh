6ebb2bf0d4f4819e9b1c9d6fc648d678b2eae602
Valter Silva <valterh@amazon.com>
2022-10-27 15:38:13 +0000

    Fix file path

--- scripts/remote/init.sh
+++ scripts/remote/init.sh
@@ -5,6 +5,6 @@ set -u
 set -o pipefail
 
 git submodule add --name habits -b main https://github.com/awslabs/aws-code-habits.git habits
-cp scripts/Makefile Makefile
+cp habits/scripts/Makefile Makefile
 make habits/install habits/init
 echo "AWS Code Habits installed successfully!"
