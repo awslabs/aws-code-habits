e65cfdd080e3e55296ad4a91f56f012ef275de73
Valter Silva <valterh@amazon.com>
2022-10-27 15:35:28 +0000

    Simplify installation

--- scripts/remote/init.sh
+++ scripts/remote/init.sh
@@ -5,10 +5,6 @@ set -u
 set -o pipefail
 
 git submodule add --name habits -b main https://github.com/awslabs/aws-code-habits.git habits
-echo 'export WORKSPACE=$(shell pwd)' | tee Makefile
-echo 'export HABITS = $(WORKSPACE)/habits' | tee --append Makefile
-echo 'include $(HABITS)/lib/make/Makefile' | tee --append Makefile
-echo 'include $(HABITS)/lib/make/*/Makefile' | tee --append Makefile
-make habits/install
-make habits/init
+cp scripts/Makefile Makefile
+make habits/install habits/init
 echo "AWS Code Habits installed successfully!"
