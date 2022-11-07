4cb2d20995225dae650d436a54f5e6f0b01afa63
Valter Silva <valterh@amazon.com>
2022-11-08 05:44:21 +0800

    Create installation only scripts

---
+++ scripts/standalone/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+git clone --branch=main --depth=1 https://github.com/awslabs/aws-code-habits.git habits
+rm -rf habits/.git
+cp habits/scripts/Makefile Makefile
+make habits/install
+echo 'habits/' >> .gitignore
+echo "AWS Code Habits installed successfully!"
