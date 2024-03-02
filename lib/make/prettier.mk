9eac8fd800c4e157ee5c457b7883a35f500629df
Valter Silva <valter.silva.au@gmail.com>
2024-03-02 09:55:59 +0000

    feat: add new targets

--- lib/make/prettier.mk
+++ lib/make/prettier.mk
@@ -1,3 +1,15 @@
 .PHONY: prettier/install
 prettier/install:
 	npm install -g prettier
+
+.PHONY: prettier/run
+prettier/run:
+	prettier --write .
+
+.PHONY: prettier/check
+prettier/check:
+	prettier --check .
+
+.PHONY: prettier/version
+prettier/version:
+	prettier --version
